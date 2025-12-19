27977582b0fe7b2ca927ac8802bb7b4a1b56c508
Shineonr <romashikl1@mail.ru>
2025-12-19 16:26:19 +0500

    Библиотека для уведомлений

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.work.runtime.ktx)
 }
